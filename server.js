b0019aef52838da12954a1b28268b1fe57789628
KritikaSrivastava <kritikasrivastava@Kritikas-MacBook-Pro.local>
2018-06-03 23:37:50 -0500

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -15,11 +17,11 @@ app.use((req,res,next) => {
     }
   });
   next();
-}); 
-
-app.use((req,res,next) => {
-  res.render('maintenance.hbs');
 });
+
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// });
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
@@ -47,6 +49,6 @@ app.get('/bad',(req,res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
